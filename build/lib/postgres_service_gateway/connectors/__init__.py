351f28ef613d6df16da6a54929da47553607679f
obaro Johnson <johnson.obaro@hotmail.com>
2022-05-08 12:04:16 +0100

    removed build folder

--- build/lib/postgres_service_gateway/connectors/__init__.py
+++
@@ -1,4 +0,0 @@
-from postgres_service_gateway.connectors.queryresult    import QueryResult
-from postgres_service_gateway.connectors.procedure      import Procedure
-from postgres_service_gateway.connectors.connector      import Option , PostgresConnector
-from postgres_service_gateway.connectors.service        import Service;
